eca7c5727f1ba292976b3783bfce5d13d7f90ac4
Venu9097 <59800433+Venu9097@users.noreply.github.com>
2023-10-28 22:49:28 +0530

    task management

---
+++ src/components/AddTask.js
@@ -0,0 +1,37 @@
+
+export const AddTask = ({tasklist, setTasklist, task, setTask}) => {
+
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        if(task.id){
+            const date = new Date();
+            const updatedTaskList = tasklist.map((todo) =>(
+                todo.id === task.id ?{id:task.id, name:task.name, time:`${date.toLocaleTimeString()} ${date.toLocaleDateString()}`} :{id:todo.id, name: todo.name, time: todo.time}
+            ));
+            task.id=''
+            task.name=''
+            setTasklist(updatedTaskList);
+        }else{
+            const date = new Date();
+        const newTask = {
+            id: date.getTime(),
+            name: e.target.task.value,
+            time: `${date.toLocaleTimeString()} ${date.toLocaleDateString()}`
+        }
+        setTasklist([...tasklist, newTask]);
+        e.target.task.value = " "
+        setTask({});
+    }
+        }
+
+
+        
+    return (
+    <section className="addTask">
+        <form onSubmit={handleSubmit}>
+            <input type="text" name="task" value={task.name || ""} autoComplete="off" placeholder="Add Task" maxLength="25" onChange={ e => setTask({...task, name: e.target.value}) } />
+            <button type="submit">{task.id ? "Update" : "Add Task"}</button>
+        </form>
+    </section>
+  )
+}
